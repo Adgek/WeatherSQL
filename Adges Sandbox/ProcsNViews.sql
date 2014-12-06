b9f765820b9311fd7a45eca48c3ea857d6d63fce
Adrian Krebs <adgekrebs@gmail.com>
2014-12-06 15:08:38 -0500

    more procs

--- Adges Sandbox/ProcsNViews.sql
+++ Adges Sandbox/ProcsNViews.sql
@@ -1,9 +1,3 @@
--- Precipition
--- 	year
--- 	month
---	area
--- 	precipition amount
-
 #*******************
 #VIEWS
 #*******************
@@ -13,19 +7,47 @@ CREATE OR REPLACE VIEW getPrecipitationView AS
 	INNER JOIN Year on Weather.YID = Year.id
 	INNER JOIN Month on Weather.MID = Month.id
 	INNER JOIN State on Weather.DID = State.id
-	ORDER BY Year.yearname, Month.monthname;
+	ORDER BY Year.yearname, Month.monthname
+
 -- Cooling heating days
+CREATE OR REPLACE VIEW getCoolAndHeatView AS
+	SELECT State.statecode, Month.monthname, Year.yearname, Weather.cdd, Weather.hdd
+	FROM Weather
+	INNER JOIN Year on Weather.YID = Year.id
+	INNER JOIN Month on Weather.MID = Month.id
+	INNER JOIN State on Weather.DID = State.id
+	ORDER BY Year.yearname, Month.monthname
 
 -- Temperature
-
+CREATE OR REPLACE VIEW getTemperatureView AS
+	SELECT State.statecode, Month.monthname, Year.yearname, Weather.Tmin, Weather.Tmax, Weather.Tavg
+	FROM Weather
+	INNER JOIN Year on Weather.YID = Year.id
+	INNER JOIN Month on Weather.MID = Month.id
+	INNER JOIN State on Weather.DID = State.id
+	ORDER BY Year.yearname, Month.monthname
 
 
 #**********************
 #Stored Procedures
 #**********************
-CREATE PROCEDURE getPrecipitationForYear @Year nvarchar(4)
+CREATE PROCEDURE getPrecipitationForArea @AreaCode int
 AS
 SELECT * 
 FROM getPrecipitationView
-WHERE Year.yearname = @Year
+WHERE State.statecode = @AreaCode
+GO
+
+CREATE PROCEDURE getCoolAndHeatForArea @AreaCode int
+AS
+SELECT * 
+FROM getCoolAndHeatView
+WHERE State.statecode = @AreaCode
+GO
+
+CREATE PROCEDURE getPrecipitationForArea @AreaCode int
+AS
+SELECT * 
+FROM getTemperatureView
+WHERE State.statecode = @AreaCode
 GO
