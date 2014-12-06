82cf435b69c2ba59eb71c43e8caa23ebb8d3c70d
ktfowler1337 <fowler_xx@hotmail.com>
2014-12-05 19:10:36 -0500

    updated script

--- Adges Sandbox/creationscript.sql
+++ Adges Sandbox/creationscript.sql
@@ -1,22 +1,21 @@
-DROP DATABASE `WeatherDB`;
+DROP DATABASE WeatherDB
 /* Create or use database*/
-CREATE DATABASE `WeatherDB`;
+CREATE DATABASE [WeatherDB]
 
-USE `WeatherDB`;
+USE WeatherDB
+GO
 
 
-CREATE TABLE `Agent` (
-  `aid` char(3) NOT NULL,
-  `aname` varchar(15),
-  `city` varchar(20),
-  `percent` int,
-  PRIMARY KEY (`aid`)
+CREATE TABLE [Agent] (
+  [aid] char(3) NOT NULL,
+  [aname] varchar(15),
+  [city] varchar(20),
+  [percent] int,
+  PRIMARY KEY ([aid])
   );
 
-/* Seed the Agent Table */
-LOCK TABLES `Agent` WRITE;
 
-insert into `Agent`(`aid`,`aname`,`city`,`percent`) 
+insert into [Agent](aid,aname,city,[percent]) 
 	values ('a01','Smith','Hamilton','6'),
 	('a02','Jones','Barrie','6'),
 	('a03','Brown','Windsor','7'),
@@ -25,5 +24,3 @@ insert into `Agent`(`aid`,`aname`,`city`,`percent`)
 	('a06','Smith','Toronto','5'),
 	('a07','Chan','Kingston','5'),
 	('a08','Oliver','Ottawa','6');
-
-UNLOCK TABLES;
